82d59029d718603bb385e7bbd374e2d59fade6e5
Amier Ariffin <muhdamierrosli@hotmail.com>
2016-12-27 16:34:42 +0800

    fixed missing user_id

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -10,7 +10,7 @@ class ListingsController < ApplicationController
 	end
 
 	def create
-		@listing = Listing.new(listing_params)
+		@listing = current_user.listings.new(listing_params)
 		if @listing.save
 			redirect_to listings_path
 		end
